b00b5c5fd4758b3792bb5b564b3e92ba57919159
santiHSS <santiagohsalazar41@gmail.com>
2025-03-27 11:40:52 +0100

    Santi1 to_do_list

---
+++ Assets/Js/main.js
@@ -0,0 +1,44 @@
+//tomamos los elementos
+let inputTarea = document.getElementById("tarea");
+let btn = document.getElementById("agregar");
+let listado = document.getElementById("listado");
+let cantidad = document.getElementById("cantidad");
+
+//variable que lleva la cantidad de tareas
+let total = 0;
+
+btn.onclick = function() {
+    //controlamos si el campo esta vacio
+    if (inputTarea.value == "") {
+        return;
+    }
+    //tomamos el valor del campo
+    let elemento = inputTarea.value;
+    //creo un elemento li
+   let li = document.createElement("li");
+    //le agrego el texto al elemento
+    li.textContent = elemento;
+    //egrego el li al listado
+    listado.appendChild(li);
+    //incremento la cantidad de tareas
+    total++;
+    cantidad.innerHTML = total;
+
+
+    //Agregamos el boton eliminar a cada elemento del listado
+    let btnEliminar = document.createElement("span");
+    btnEliminar.textContent = "\u00d7";
+    li.appendChild(btnEliminar);
+
+
+    //Agregamos la funcionalidad que elimina del listado el elemento
+    btnEliminar.onclick = function() {
+        li.remove();
+        total--;
+        cantidad.innerHTML = total;
+    }
+
+    //limpiamos el campo
+    inputTarea.value = "";
+
+}
